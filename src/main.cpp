461dc1927183adfa6931a286c968488cb935e578
SamiyelF <samiyelfrazier@gmail.com>
2025-01-04 01:21:43 -0800

    tournament re-added

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,51 @@ void printnav() {
 int main() {
   Thread VELOCITY([]() { motors::Velocity(tank.m_pass); });
   Thread TIMER([]() { misc::Timer(); });
-  TIMER.Run();
   VELOCITY.Run();
-  printnav();
-  nav.GoTo(P2D(6, 0), 5);
-  printnav();
-  nav.GoTo(P2D(0, 0), 5);
-  printnav();
-  while (1) {
-    nav.GoTo(P2D(0, 0), 1);
-    printnav();
+  claw.Set(90);
+  arm.Set(180);
+  if (true /*tournament mode*/) {
+    misc::WaitForLight(startlight.m_Port);
+    shut_down_in(119);
   }
+  TIMER.Run();
+  while (ground.Value() < 0.92) {
+    tank.Speed(-100, -20, 0.1);
+  }
+  tank.Speed(-100, -20, 1);
+  while (ground.Value() < 0.92) {
+    tank.Speed(-100, 20, 0.1);
+  }
+  Wait(.5);
+  /* REORIENTED */
+  tank.Speed(85, 100, 1.5);
+  tank.Speed(100, 15, 1.18);
+  /* HALLWAY */
+  long t1 = BKND::G_CurrentMS;
+  while (ground.Value() < 0.92) {
+    tank.Speed(50, 50, 0.1);
+  }
+  long t2 = BKND::G_CurrentMS;
+  tank.Speed(50, 50, ((float)(t2 - t1) / 1000) - 2);
+  tank.Speed(0, 100, 1.25);
+  tank.Speed(-75, -25, 1.5);
+  tank.Speed(-50, -50, 8);
+  Wait(5);
+  /* IN STARTING BOX */
+  tank.Speed(50, 50, 2);
+  tank.Speed(50, -50, 3);
+  tank.Speed(-75, -75, 3);
+  arm.Set(45);
+  tank.Speed(-75, -75, 1);
+  Wait(1);
+  claw.Set(0);
+  Wait(1);
+  arm.Set(180);
+  Wait(1);
+  tank.Speed(75, 75, 4);
+  tank.Speed(0, 50, 3);
+  tank.Speed(50, 50, 2);
+  arm.Set(0);
+  claw.Set(90);
   return 0;
 }
